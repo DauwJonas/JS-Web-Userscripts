8a715aa3f33ed44d127ea3ca2abc06248a2b71fc
Jonas Dauw <jonas.dauw@student.odisee.be>
2021-09-13 13:34:59 +0200

    Update VRC

--- VRChat/Auto Launch & Invite/Auto Invite.js
+++ VRChat/Auto Launch & Invite/Auto Invite.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         VRC Auto Invite
-// @namespace    
+// @namespace    https://github.com/DauwJonas/JS-Web-Userscripts/tree/master/VRChat/Auto%20Launch%20%26%20Invite
 // @version      0.1
-// @description  
+// @description  Auto invite using the web interface.
 // @author       Dauw_Jonas
 // @match        https://vrchat.com/home/launch?worldId=wrld_*
 // @icon         https://www.google.com/s2/favicons?domain=vrchat.com
@@ -12,10 +12,16 @@
 (function() {
     'use strict';
 
+    ////Settings
+    let launchDelay = 5; //In Seconds
+
+    //Format settings 
+    launchDelay = launchDelay * 1000;
+
     //Insert UI
     let ALW_Body = document.createElement("div");
     ALW_Body.classList.add("ALW_Body");
-    ALW_Body.innerHTML = "<style>\/*****  General*\/ :root {\/* General *\/--DJ-background-color: #191919;--DJ-background-color2: #262626;\/* Text *\/--DJ-color: #afafaf;--DJ-font-family: inherit;--DJ-font-size: 2rem;\/** Link *\/\/* Input Text *\/--DJ-input-text-background-color: #262626;--DJ-input-text-border-color: #34393D;\/** Button *\/--DJ-button-background-color: #82471A;--DJ-button-border-color: #592D10;--DJ-button-color: #D1D1D1;}p {margin: 0;margin-bottom: .25rem;}\/*****  Body*\/.ALW_Body {width: 100%;align-items: center;display: flex;flex-direction: column;flex-wrap: nowrap;justify-content: center;z-index: 10000;}.ALW_Body>* {font-family: var(--DJ-font-family);color: var(--DJ-color);}.ALW_UI {display: block;background-color: var(--DJ-background-color);border: 2px solid var(--DJ-background-color2);border-radius: 2px;padding: .5rem;position: fixed;top: 0;z-index: 10000;}.ALW_UI>div {margin-bottom: .5rem;}.ALW_UI div:last-child {margin-bottom: 0;}\/****  Header*\/.ALW_Header {}.ALW_h1 {font-size: 125%;font-weight: bold;text-align: center;}.ALW_Credits {display: none;font-size: 80%;text-align: right;}\/****  Status*\/.ALW_Status {margin-bottom: .5rem;}\/****  Cancel*\/.ALW_Btn {background-color: #8b0000;border: 2px solid #640000;border-radius: .5rem;color: var(--DJ-button-color);display: block;font-weight: bold;margin: 0 auto;padding: .3rem;text-align: center;transition-duration: .1s;width: fit-content;min-width: 35%;}.ALW_Btn:hover,.ALW_Btn:focus {background-color: #640000;border: 2px solid #8b0000;cursor: pointer;font-weight: bold;}<\/style><div class=\"ALW_UI\"><div class=\"ALW_Header\"><p class=\"ALW_h1\">Auto Launch Web<\/p><p class=\"ALW_Credits\">By Dauw_Jonas<\/p><\/div><div class=\"ALW_Status\"><span>Status: <\/span><span id=\"ALW_Status\"><\/span><span id=\"ALW_Status2\"><\/span><span>.<\/span><\/div><div class=\"ALW_Cancel\"><p class=\"ALW_p\">Stop auto launching of a instants in web.<\/p><p class=\"ALW_Btn\" id=\"ALW_Cancel\">Cancel<\/p><\/div><\/div>";
+    ALW_Body.innerHTML = "<style>\/*****  General*\/ :root {\/* General *\/--DJ-background-color: #191919;--DJ-background-color2: #262626;\/* Text *\/--DJ-color: #afafaf;--DJ-font-family: inherit;--DJ-font-size: 2rem;\/** Link *\/\/* Input Text *\/--DJ-input-text-background-color: #262626;--DJ-input-text-border-color: #34393D;\/** Button *\/--DJ-button-background-color: #82471A;--DJ-button-border-color: #592D10;--DJ-button-color: #D1D1D1;}p {margin: 0;margin-bottom: .25rem;}\/*****  Body*\/.ALW_Body {width: 100%;align-items: center;display: flex;flex-direction: column;flex-wrap: nowrap;justify-content: center;z-index: 10000;}.ALW_Body>* {font-family: var(--DJ-font-family);color: var(--DJ-color);}.ALW_UI {display: block;background-color: var(--DJ-background-color);border: 2px solid var(--DJ-background-color2);border-radius: 2px;padding: .5rem;position: fixed;top: 0;z-index: 10000;}.ALW_UI>div {margin-bottom: .5rem;}.ALW_UI div:last-child {margin-bottom: 0;}\/****  Header*\/.ALW_Header {}.ALW_h1 {font-size: 125%;font-weight: bold;text-align: center;}.ALW_Credits {display: none;font-size: 80%;text-align: right;}\/****  Status*\/.ALW_Status {margin-bottom: .5rem;}\/****  Cancel*\/.ALW_Btn {background-color: #8b0000;border: 2px solid #640000;border-radius: .5rem;color: var(--DJ-button-color);display: block;font-weight: bold;margin: 0 auto;padding: .3rem;text-align: center;transition-duration: .1s;width: fit-content;min-width: 35%;}.ALW_Btn:hover,.ALW_Btn:focus {background-color: #640000;border: 2px solid #8b0000;cursor: pointer;font-weight: bold;}<\/style><div class=\"ALW_UI\"><div class=\"ALW_Header\"><p class=\"ALW_h1\">Auto Launch Web<\/p><p class=\"ALW_Credits\">By Dauw_Jonas<\/p><\/div><div class=\"ALW_Status\"><span>Status: <\/span><span id=\"ALW_Status\"><\/span><span id=\"ALW_Status2\"><\/span><span>.<\/span><\/div><div class=\"ALW_Cancel\"><p class=\"ALW_p\">Stop auto launching of a instance in web.<\/p><p class=\"ALW_Btn\" id=\"ALW_Cancel\">Cancel<\/p><\/div><\/div>";
     //Insert UI
     document.body.appendChild(ALW_Body);
     //Var UI
@@ -42,7 +48,6 @@
     //Times
     const checkPageLoadedDelay = 2000; //In ms
     let countPageNotLoaded = 0;
-    const launchDelay = 5000;
     let launchIn;
     //Wait for page to load
     timeOutFunction = setTimeout(checkPageLoaded, checkPageLoadedDelay);
@@ -86,7 +91,6 @@
             const element = VRCWebEls[i];
             element.click();
         }
-        close();
         ALW_Status.innerText = "Done";
         ALW_Status2.innerText = "";
         //End script
--- VRChat/Auto Launch & Invite/README.md
+++ VRChat/Auto Launch & Invite/README.md
@@ -1,1 +1,31 @@
 # VRC-Auto-Launch-in-web-and-invite
+
+Sometimes I look for worlds on the VRChat website. And to easily play in that world I need to launch them. But the settings you put in don’t get saved. So I have to reinput them in if I want my desired settings. Otherwise I can just launch it with the default settings. Which is Public, US. But I prefer Friends+ and I’m from the EU.
+Thanks to this script I can scroll through the world list and open every world I want to visit in a new tab and it will automatically launch the instance with the desired settings. Then it will automatically invite myself and predownload/cache the world using VRCX <https://github.com/pypy-vrc/VRCX>.  (Note: Make sure you have increased your cache size if you are planning to predownload a lot of worlds, otherwise VRChat might delete them because of a lack of space and thus you have wasted that download. Info on how to change cache settings (and other stuff) <https://docs.vrchat.com/docs/configuration-file>.)
+Then the tab will remain so I can invite myself when I wanna to that world.
+
+## Auto Launch Web
+
+Auto make a new instance using the web interface.
+
+You can change the following settings in the JS file under ////Settings
+
+* Instance Type:
+  * The type of the instance: friends+, friends, invite+, invite.
+  * Var name: "instanceType".
+* Region: region = "eu";
+  * The region of the instance: us, eu, jp. (Future regions might be automaticity supported.)
+  * Var name: "region".
+* Launch Delay:
+  * How long it will take before the script does it thing. Value in seconds.
+  * Var name: "launchDelay".
+
+## Auto Invite
+
+Auto invite using the web interface.
+
+You can change the following settings in the JS file under ////Settings
+
+* Launch Delay:
+  * How long it will take before the script does it thing. Value in seconds.
+  * Var name: "launchDelay".
