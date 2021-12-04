f6b729c0b9aa143558ed1acadeb78d7b05837b3d
Jonas Dauw <jonas.dauw@student.odisee.be>
2021-12-04 22:26:28 +0100

    Update 2deHands Auto Extend.js

--- 2deHands/2deHands Auto Extend.js
+++ 2deHands/2deHands Auto Extend.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         2deHands Auto Extend
-// @namespace    
+// @namespace    https://github.com/DauwJonas/JS-Web-Userscripts/tree/master/2deHands
 // @version      0.1
 // @description  Auto extend all expiring.
 // @author       Dauw_Jonas
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://github.com/DauwJonas/JS-Web-Userscripts/blob/master/2deHands/2deHands%20Auto%20Extend.js
+// @downloadURL  https://github.com/DauwJonas/JS-Web-Userscripts/blob/master/2deHands/2deHands%20Auto%20Extend.js
 // @match        https://www.2dehands.be/my-account/sell/index.html
 // @icon         https://www.google.com/s2/favicons?domain=2dehands.be
 // @grant        none
