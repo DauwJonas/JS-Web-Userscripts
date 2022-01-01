cdf1c8e1118fa8be3e2c4ac3112f922238fcb0e9
Jonas Dauw <jonas.dauw@student.odisee.be>
2022-01-01 19:47:17 +0100

    Update 2deHands

--- 2deHands/2deHands Auto Extend.js
+++ 2deHands/2deHands Auto Extend.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         2deHands Auto Extend
 // @namespace    https://github.com/DauwJonas/JS-Web-Userscripts/tree/master/2deHands
-// @version      0.1
+// @version      0.2
 // @description  Auto extend all expiring.
 // @author       Dauw_Jonas
-// @updateURL    https://github.com/DauwJonas/JS-Web-Userscripts/blob/master/2deHands/2deHands%20Auto%20Extend.js
-// @downloadURL  https://github.com/DauwJonas/JS-Web-Userscripts/blob/master/2deHands/2deHands%20Auto%20Extend.js
+// @updateURL    https://raw.githubusercontent.com/DauwJonas/JS-Web-Userscripts/master/2deHands/2deHands%20Auto%20Extend.js
+// @downloadURL  https://raw.githubusercontent.com/DauwJonas/JS-Web-Userscripts/master/2deHands/2deHands%20Auto%20Extend.js
 // @match        https://www.2dehands.be/my-account/sell/index.html
 // @icon         https://www.google.com/s2/favicons?domain=2dehands.be
 // @run-at       document-end
@@ -15,5 +15,13 @@
 (function() {
     'use strict';
 
-    document.getElementsByClassName("verlengen").forEach(element => element.click());
+    DoExtent();
+    setTimeout(DoExtent, 1000);
+    setTimeout(DoExtent, 5000);
+    setTimeout(DoExtent, 10000);
+
+    function DoExtent() {
+        document.getElementsByClassName("verlengen").forEach(element => element.click());
+        console.log("Extent clicked.")
+    }
 })();
