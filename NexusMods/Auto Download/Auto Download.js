8481d68dd0c2b694ce61863ccc17176001e6b45b
Jonas Dauw <jonas.dauw@student.odisee.be>
2022-05-06 01:00:17 +0200

    NexusMods/Auto Download
    * Fix match URL for all games.
    * Add ReadMe.

--- NexusMods/Auto Download/Auto Download.js
+++ NexusMods/Auto Download/Auto Download.js
@@ -6,7 +6,7 @@
 // @author       Dauw_Jonas
 // @downloadURL  https://github.com/DauwJonas/JS-Web-Userscripts/raw/master/NexusMods/Auto%20Download/Auto%20Download.js
 // @updateURL    https://github.com/DauwJonas/JS-Web-Userscripts/raw/master/NexusMods/Auto%20Download/Auto%20Download.js
-// @match        https://www.nexusmods.com/newvegas/mods/*
+// @match        https://www.nexusmods.com/*/mods/*
 // @icon         https://images.nexusmods.com/favicons/DeepOrange/favicon-230x230.png
 // @run-at       document-end
 // @grant        none
---
+++ NexusMods/Auto Download/README.md
@@ -0,0 +1,3 @@
+# NexusMods Auto Download
+
+When you go on a page with url `https://www.nexusmods.com/*/mods/*` it will click the `Slow download` for you.
