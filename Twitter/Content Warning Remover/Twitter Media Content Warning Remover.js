28db786b211bc8cf9fefa5d08afb9a417d1d1866
Dauw_Jonas <43506590+DauwJonas@users.noreply.github.com>
2022-05-30 00:41:54 +0200

    Update Twitter Media Content Warning Remover.js

--- Twitter/Content Warning Remover/Twitter Media Content Warning Remover.js
+++ Twitter/Content Warning Remover/Twitter Media Content Warning Remover.js
@@ -3,11 +3,13 @@
 // @namespace    https://github.com/DauwJonas/JS-Web-Userscripts/tree/master/Twitter/Content%20Warning%20Remover
 // @version      1
 // @description  Remove content warning from tweets
-// @author       sapphire & Dauw_Jonas
+// @author       sapphire - https://www.sapphire.sh/
+// @author       Dauw_Jonas
 // @updateURL    https://raw.githubusercontent.com/DauwJonas/JS-Web-Userscripts/master/Twitter/Content%20Warning%20Remover/Twitter%20Media%20Content%20Warning%20Remover.js
 // @match        https://twitter.com/*
 // @match        https://mobile.twitter.com/*
 // @match        https://platform.twitter.com/*
+// @icon         https://abs.twimg.com/responsive-web/client-web/icon-svg.168b89d5.svg
 // @grant        none
 // @run-at       document-end
 // @license      MIT
