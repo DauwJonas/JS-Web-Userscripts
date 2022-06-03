40f6c9b05de614b81b7c25156aaea052238bed9c
Dauw_Jonas <43506590+DauwJonas@users.noreply.github.com>
2022-06-04 00:00:40 +0200

    Update Twitter Media Content Warning Remover.js
    Removed spaces

--- Twitter/Content Warning Remover/Twitter Media Content Warning Remover.js
+++ Twitter/Content Warning Remover/Twitter Media Content Warning Remover.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name         Twitter Media Content Warning Remover
-// @namespace    https://github.com/DauwJonas/JS-Web-Userscripts/tree/master/Twitter/Content%20Warning%20Remover
-// @version      1
-// @description  Remove content warning from tweets
-// @author       sapphire - https://www.sapphire.sh/
-// @author       Dauw_Jonas
-// @updateURL    https://raw.githubusercontent.com/DauwJonas/JS-Web-Userscripts/master/Twitter/Content%20Warning%20Remover/Twitter%20Media%20Content%20Warning%20Remover.js
-// @downloadURL  https://raw.githubusercontent.com/DauwJonas/JS-Web-Userscripts/master/Twitter/Content%20Warning%20Remover/Twitter%20Media%20Content%20Warning%20Remover.js
-// @match        https://twitter.com/*
-// @match        https://mobile.twitter.com/*
-// @match        https://platform.twitter.com/*
-// @icon         https://abs.twimg.com/favicons/twitter.ico
-// @grant        none
-// @run-at       document-end
-// @license      MIT
+// @name        Twitter Media Content Warning Remover
+// @namespace   https://github.com/DauwJonas/JS-Web-Userscripts/tree/master/Twitter/Content%20Warning%20Remover
+// @version     1
+// @description Remove content warning from tweets
+// @author      sapphire - https://www.sapphire.sh/
+// @author      Dauw_Jonas
+// @updateURL   https://raw.githubusercontent.com/DauwJonas/JS-Web-Userscripts/master/Twitter/Content%20Warning%20Remover/Twitter%20Media%20Content%20Warning%20Remover.js
+// @downloadURL https://raw.githubusercontent.com/DauwJonas/JS-Web-Userscripts/master/Twitter/Content%20Warning%20Remover/Twitter%20Media%20Content%20Warning%20Remover.js
+// @match       https://twitter.com/*
+// @match       https://mobile.twitter.com/*
+// @match       https://platform.twitter.com/*
+// @icon        https://abs.twimg.com/favicons/twitter.ico
+// @grant       none
+// @run-at      document-end
+// @license     MIT
 // ==/UserScript==
 /******/
 (() => { // webpackBootstrap
